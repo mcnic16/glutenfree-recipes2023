e55273da2a1c6b4eed75ca75ef3b088616275879
mcnic16 <jamesmcnic@yahoo.co.uk>
2023-02-05 17:10:40 +0000

    deleted glutenfree folder, run.py and routes.py and updated app.py

--- glutenfree/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import render_template
-from glutenfree import app, db
-from glutenfree.models import Starter, Main, Dessert, Drink
-
-
-@app.route("/")
-def home():
-    return render_template("base.html")
